527417c027d8464df237630de8c58b7262e474a9
havi-kim <hyochankim94@gmail.com>
2023-02-26 05:57:10 +0900

    feat: Add UniswapV2 connector

--- contracts/position/Connection.sol
+++ contracts/position/Connection.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Connection {
+import "../../interfaces/IConnection.sol";
+
+contract Connection is IConnection{
     address public balancer;
 
     constructor() public {
@@ -16,22 +18,7 @@ contract Connection {
 
     /// @dev Call to the target using the given data.
     /// @param _data The data used in the call.
-    function execute(bytes calldata _data) external onlyBalancer {
-        executeInternal(_data);
-    }
-
-    /// @dev Call batch to the target using the given data array.
-    /// @param _dataArray The data array used in the call.
-    function executeBatch(bytes[] calldata _dataArray) external onlyBalancer {
-        for (uint256 idx = 0; idx < _dataArray.length; idx ++) {
-            executeInternal(dex, _dataArray[idx]);
-        }
-    }
-
-    /// @dev Internal function call to the target using the given data.
-    /// @param _target The target contract address to call.
-    /// @param _data The data used in the call.
-    function executeInternal(address _target, bytes calldata _data) internal {
+    function execute(address _target, bytes calldata _data) external onlyBalancer returns (bytes){
         (bool ok, bytes memory returndata) = _target.call(_data);
         if (!ok) {
             if (returndata.length > 0) {
@@ -43,5 +30,6 @@ contract Connection {
                 revert('bad execute call');
             }
         }
+        return returndata;
     }
 }
